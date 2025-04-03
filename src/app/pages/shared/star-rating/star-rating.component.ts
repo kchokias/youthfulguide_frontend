28617d9c8efbc0609bfcb9eae766d82e018014c4
G979 <57722536+G979@users.noreply.github.com>
2025-04-03 22:00:23 +0300

    Bookings page almost there 03.04

--- src/app/pages/shared/star-rating/star-rating.component.ts
+++ src/app/pages/shared/star-rating/star-rating.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class StarRatingComponent {
 
   @Input() rating: number = 0;
+  @Input() fontSize: string = '2em';
 
   get stars() {
     return Array(Math.floor(this.rating)).fill(0);
